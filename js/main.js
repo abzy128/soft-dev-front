cade452395401171dff70461e6f5ed4f00077a31
Abzal Orazbek <abzalorazbek2003@gmail.com>
2023-11-24 03:31:03 +0600

    Fix Color theme JS

--- js/main.js
+++ js/main.js
@@ -1,29 +1,14 @@
-// Import the Bootstrap bundle
-//
-// This includes Popper and all of Bootstrap's JS plugins.
-
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
-
-//
-// Place any custom JS here
-//
-
-// Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new bootstrap.Popover(popover)
-  })
-  $("#theme-switch-button").on("click", function (e) {
-    e.preventDefault();
-    if ($("html").attr("data-bs-theme") == "dark") {
-      $("html").attr("data-bs-theme", "light");
-      $("#theme-switch-button").removeClass("fa-sun");
-      $("#theme-switch-button").addClass("fa-moon");
-    } else {
-      $("html").attr("data-bs-theme", "dark");
-      $("#theme-switch-button").removeClass("fa-moon");
-      $("#theme-switch-button").addClass("fa-sun");
-    }
-  });
-  
+$("#theme-switch-button").on("click", function (e) {
+  e.preventDefault();
+  if ($("html").attr("data-bs-theme") == "dark") {
+    $("html").attr("data-bs-theme", "light");
+    $("#theme-switch-button").removeClass("fa-sun");
+    $("#theme-switch-button").addClass("fa-moon");
+  } else {
+    $("html").attr("data-bs-theme", "dark");
+    $("#theme-switch-button").removeClass("fa-moon");
+    $("#theme-switch-button").addClass("fa-sun");
+  }
+});
